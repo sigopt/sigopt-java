eff49ac85f3dba8298a08e97cb3c7a431a37d744
Patrick Hayes <pfhayes@gmail.com>
2016-06-17 14:33:22 -0700

    2.1.0
    - Fix null value serialization. Previously all keys missing from objects
      were serialized as nulls. This meant it was impossible to make some API calls
      which required keys to be absent.

--- src/main/java/com/sigopt/Sigopt.java
+++ src/main/java/com/sigopt/Sigopt.java
@@ -6,7 +6,7 @@ public abstract class Sigopt {
     public static volatile String apiVersion = "v1";
 
     public static final String CLIENT_AUTH_PARAM = "sample_client_token";
-    public static final String VERSION = "2.0.1";
+    public static final String VERSION = "2.1.0";
     public static final String SUPPORT_EMAIL = "support@sigopt.com";
     public static final String DOCS_URL = "https://sigopt.com/docs/overview";
 
